aa94d7a3a02ed9449b793551968aacacfab7ea3e
foo15 <53960938+foo15@users.noreply.github.com>
2020-12-28 12:52:02 -0500

    added proxy class

---
+++ Omegle-Bot/Helpers/Proxy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+
+namespace Omegle_Bot.Utils
+{
+    public sealed class Proxy
+    {
+        public IPAddress IP_ADDRESS { get; private set; }
+
+        public int PORT { get; private set; }
+
+        public Proxy(IPAddress ip, int port)
+        {
+            IP_ADDRESS = ip;
+            PORT = port;
+        }
+        public override string ToString()
+        {
+            return IP_ADDRESS.ToString() + ":" + PORT.ToString();
+        }
+    }
+}
--- Omegle-Bot/MainWindow.xaml.cs
+++ Omegle-Bot/MainWindow.xaml.cs
@@ -1,28 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+using Omegle_Bot.Utils;
 
 namespace Omegle_Bot
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonAddProxy_Click(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Proxy List (.txt)|*.txt";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                string[] lines = File.ReadAllLines(dlg.FileName);
+
+                foreach (String line in lines)
+                {
+
+                }
+            }
         }
     }
 }
